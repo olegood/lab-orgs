d34e3603b23e8e297c003bda88b03b5402ba9dda
Oleg Anastassov <oleg.anastassov@gmail.com>
2026-02-08 17:33:40 +0100

    chore: move jpa configuration to separate class

---
+++ src/main/java/olegood/lab/orgs/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package olegood.lab.orgs.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {}
