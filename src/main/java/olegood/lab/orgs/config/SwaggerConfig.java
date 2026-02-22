21c265b1b006bb02da2679156e710ad9e10fc1d9
Oleg Anastassov <oleg.anastassov@gmail.com>
2026-02-23 00:19:21 +0100

    chore: use build properties in swagger config

--- src/main/java/olegood/lab/orgs/config/SwaggerConfig.java
+++ src/main/java/olegood/lab/orgs/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package olegood.lab.orgs.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,12 +10,12 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
   @Bean
-  public OpenAPI orgsOpenAPI() {
+  public OpenAPI orgsOpenAPI(BuildProperties build) {
     return new OpenAPI()
         .info(
             new Info()
-                .title("Orgs API")
+                .title(build.getName())
                 .description("API for managing organizations and enrollments")
-                .version("1.0.0"));
+                .version(build.getVersion()));
   }
 }
